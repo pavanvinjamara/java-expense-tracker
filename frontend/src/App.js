16452ad2d4fe894de6f51e06c01dd484ba916988
vinjamara pavan kumar <pavanvinjamara323@gmail.com>
2026-03-13 19:20:54 +0530

    compeleted setup backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App bg-red-500">
-        <p>Pavan</p>
-    </div>
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+
+        <Route path="/" element={<Home/>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
